ab70cb350c6a3363642ce196046d936af1ef0249
canjtosun <cantosun@icloud.com>
2020-03-04 20:37:27 -0800

    project

---
+++ BloodBank/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BloodBank
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            MySqlConnection cnn;
+            connetionString = "server=remotemysql.com;port=3306;database=Kg788Sv4Ht;uid=Kg788Sv4Ht;pwd=NDOIFG2Huc;";
+            cnn = new MySqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+    }
+}
